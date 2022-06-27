beef1e4bd8c4e9009dec8044d3196a8e76f5fd24
ka52k <76809740+ka52k@users.noreply.github.com>
2022-06-27 12:56:46 +0900

    セクション6-30迄

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+// import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/Searchinput";
+// import { UserCard } from "./components/organisms/user/UserCard";
+// import { DefaultLayout } from "./components/templates/DefaultLayout";
+// import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
+// import { Router } from "./router/Router";
+// import { UserProvider } from "./providers/UserProvider";
+// import { RecoilRoot } from "recoil";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
     </div>
+    // <BrowserRouter>
+    //     <DefaultLayout>
+    // <PrimaryButton>テスト</PrimaryButton>
+    //       {/* <PrimaryButton>検索</PrimaryButton> */}
+    //       <SecondaryButton>検索</SecondaryButton>
+    //       <br />
+    //       <SearchInput />
+    //       <UserCard user={user} />
+    //     </DefaultLayout>
+    // </BrowserRouter>
+    // <RecoilRoot>
+    //   <UserProvider>
+    //     <Router />
+    //   </UserProvider>
+    // </RecoilRoot>
   );
 }
